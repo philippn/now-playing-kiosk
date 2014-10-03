839a74bb1cb5f9e4337659b67a05180781b980e5
Philipp Nanz <philippn@gmail.com>
2014-10-03 12:48:44 +0200

    Bugfix: No track info displayed while context menu is opened

--- NowPlayingKiosk/SpotifyNowPlayingTrackInfoProvider.cs
+++ NowPlayingKiosk/SpotifyNowPlayingTrackInfoProvider.cs
@@ -5,35 +5,49 @@ namespace NowPlayingKiosk
 {
     public class SpotifyNowPlayingTrackInfoProvider : NowPlayingTrackInfoProvider
     {
+        private TrackInfo lastPlayed;
+
         public TrackInfo WhatIsNowPlaying()
         {
             // Get the process
             Process[] processList = Process.GetProcessesByName("spotify");
 
-            string windowTitle = null;
             string wantedPrefix = "Spotify - ";
             foreach (Process process in processList)
             {
-                if (!String.IsNullOrEmpty(process.MainWindowTitle) && process.MainWindowTitle.StartsWith(wantedPrefix))
+                if (!String.IsNullOrEmpty(process.MainWindowTitle) && process.MainWindowTitle.Equals("Spotify"))
                 {
-                    windowTitle = process.MainWindowTitle.Substring(wantedPrefix.Length);
+                    // Nothing playing right now
+                    lastPlayed = null;
+                    return null;
+                }
+                else if (!String.IsNullOrEmpty(process.MainWindowTitle) && process.MainWindowTitle.StartsWith(wantedPrefix))
+                {
+                    // Spotify is playing something
+                    string windowTitle = process.MainWindowTitle.Substring(wantedPrefix.Length);
+                    TrackInfo nowPlaying = null;
+                    if (windowTitle != null)
+                    {
+                        // For example "Foo Fighters – Monkey Wrench"
+                        int idx = windowTitle.IndexOf('–');
+                        if (idx != -1)
+                        {
+                            string artist = windowTitle.Substring(0, idx).Trim();
+                            string title = windowTitle.Substring(idx + 1).Trim();
+                            nowPlaying = new TrackInfo(artist, title);
+                        }
+                    }
+
+                    lastPlayed = nowPlaying;
+                    return nowPlaying;
+                }
+                else if (String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    return lastPlayed;
                 }
             }
 
-            TrackInfo nowPlaying = null;
-            if (windowTitle != null)
-            {
-                // For example "Foo Fighters – Monkey Wrench"
-                int idx = windowTitle.IndexOf('–');
-                if (idx != -1)
-                {
-                    string artist = windowTitle.Substring(0, idx).Trim();
-                    string title = windowTitle.Substring(idx + 1).Trim();
-                    nowPlaying = new TrackInfo(artist, title);
-                }
-            }
-
-            return nowPlaying;
+            return null;
         }
     }
 }
